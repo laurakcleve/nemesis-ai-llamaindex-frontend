395d994295b78f54df33a4c67434f260b7811ed3
laura <laurakcleve@gmail.com>
2024-04-18 15:05:51 -0600

    test env variable

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
 
   async function ask(question: string) {
     setIsLoading(true)
+    console.log(import.meta.env.VITE_SERVER_URL)
     fetch(import.meta.env.VITE_SERVER_URL, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
